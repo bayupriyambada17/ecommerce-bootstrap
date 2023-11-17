764e17d6c43dc12a129e8f7908e37f96a3871149
yamayuba00 <yamayuba00@gmail.com>
2023-11-17 23:23:44 +0700

    Mengubah query select kategori, toko, penambahan min harga, maks harga - sidebar

---
+++ assets/js/query/helper.js
@@ -0,0 +1,17 @@
+function formatRp(input) {
+  if (input) {
+    // Remove non-numeric characters
+    let value = input.value.replace(/\D/g, '');
+
+    // Convert to a BigInt and then to a string to handle large numbers
+    value = BigInt(value).toLocaleString('id-ID');
+
+    // Format as Rupiah currency
+    value = value;
+
+    // Update the input value
+    input.value = value;
+  }
+}
+
+formatRp();
--- assets/js/query/script.js
+++ assets/js/query/script.js
@@ -1,14 +1,61 @@
+"use strict"
+
 const count = document.getElementById('count');
+const categorySelect = document.getElementById('categorySelect')
+const shopSelect = document.getElementById('shopSelect');
+
+// get API
+const urlApi = 'https://bootcamp-api.bayupriyambada.repl.co/api/v1/productsApi.json';
 
 async function getProducts() {
-  const products = await fetch('https://bootcamp-api.bayupriyambada.repl.co/api/v1/productsApi.json');
+  const products = await fetch(urlApi);
   const prodApi = await products.json();
-
   // hitung total jumlah produk
-  count.innerText = prodApi.products.length;
+  count.innerText = prodApi.products.length || 0; // jika ada data total atau 0 data
 }
+
+async function getCategory() {
+  const categoryProducts = await fetch(urlApi);
+  const category = await categoryProducts.json();
+
+  // jika terjadi category sama maka jadikan 1 saja.
+  const uniqueCategories = [...new Set(category.products.map(cat => cat.category))];
+  uniqueCategories.unshift('Pilihan Kategori');
+
+  const selectUniques = uniqueCategories.map((category) => getCategoryProd(category)).join('');
+  categorySelect.innerHTML = selectUniques;
+}
+
+const getCategoryProd = (category) => {
+  const lowercaseKey = category.toLowerCase().replace(/ /g, '-');
+  return `
+  <option value="${lowercaseKey}">${category}</option>
+  `
+}
+
+// Pilihan Toko
+async function getShopName() {
+  const shopProducts = await fetch(urlApi);
+  const shop = await shopProducts.json();
+  const uniqueShopName = [...new Set(shop.products.map(s => s.shop))];
+  uniqueShopName.unshift('Pilihan Toko');
+
+  const selectUniques = uniqueShopName.map((shop) => getShopNameProd(shop)).join('');
+  shopSelect.innerHTML = selectUniques;
+}
+
+const getShopNameProd = (shopName) => {
+  const lowercaseKey = shopName.toLowerCase().replace(/ /g, '-');
+
+  return `
+  <option value="${lowercaseKey}">${shopName}</option>
+  `
+}
+
 function init() {
   getProducts();
+  getCategory();
+  getShopName();
 }
 
 init();
