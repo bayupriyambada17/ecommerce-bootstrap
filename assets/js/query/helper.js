3695e3e0334bfdf211d00570b63c7cd32bcfd9e2
yamayuba00 <yamayuba00@gmail.com>
2023-11-17 23:58:02 +0700

    modifikasi query js dengan lebih singkat

--- assets/js/query/helper.js
+++ assets/js/query/helper.js
@@ -1,3 +1,5 @@
+export const urlApi = 'https://bootcamp-api.bayupriyambada.repl.co/api/v1/productsApi.json'
+
 function formatRp(input) {
   if (input) {
     // Remove non-numeric characters
--- assets/js/query/script.js
+++ assets/js/query/script.js
@@ -1,47 +1,48 @@
 "use strict"
+import { urlApi } from './helper.js'
 
 const count = document.getElementById('count');
 const categorySelect = document.getElementById('categorySelect')
 const shopSelect = document.getElementById('shopSelect');
 
-// get API
-const urlApi = 'https://bootcamp-api.bayupriyambada.repl.co/api/v1/productsApi.json';
+async function fetchData() {
+  const response = await fetch(urlApi);
+  return await response.json();
+}
 
 async function getProducts() {
-  const products = await fetch(urlApi);
-  const prodApi = await products.json();
+  const { products } = await fetchData();
   // hitung total jumlah produk
-  count.innerText = prodApi.products.length || 0; // jika ada data total atau 0 data
+  count.innerText = products.length || 0; // jika ada data total atau 0 data
 }
 
 async function getCategory() {
-  const categoryProducts = await fetch(urlApi);
-  const category = await categoryProducts.json();
+  const { products } = await fetch(urlApi);
 
-  // jika terjadi category sama maka jadikan 1 saja.
-  const uniqueCategories = [...new Set(category.products.map(cat => cat.category))];
-  uniqueCategories.unshift('Pilihan Kategori');
+  if (products) {
+    // jika terjadi category sama maka jadikan 1 saja.
+    const uniqueCategories = [...new Set(products.map(prod => prod.category))];
+    uniqueCategories.unshift('Pilihan Kategori');
 
-  const selectUniques = uniqueCategories.map((category) => getCategoryProd(category)).join('');
-  categorySelect.innerHTML = selectUniques;
+    categorySelect.innerHTML = uniqueCategories.map((category) => getCategoryProd(category)).join('');
+
+  }
 }
 
 const getCategoryProd = (category) => {
   const lowercaseKey = category.toLowerCase().replace(/ /g, '-');
-  return `
-  <option value="${lowercaseKey}">${category}</option>
-  `
+  return `<option value="${lowercaseKey}">${category}</option>`
 }
 
 // Pilihan Toko
 async function getShopName() {
-  const shopProducts = await fetch(urlApi);
-  const shop = await shopProducts.json();
-  const uniqueShopName = [...new Set(shop.products.map(s => s.shop))];
-  uniqueShopName.unshift('Pilihan Toko');
+  const { products } = await fetch(urlApi);
+  if (products) {
+    const uniqueShopName = [...new Set(products.map(s => s.shop))];
+    uniqueShopName.unshift('Pilihan Toko');
+    shopSelect.innerHTML = uniqueShopName.map((shop) => getShopNameProd(shop)).join('');;
 
-  const selectUniques = uniqueShopName.map((shop) => getShopNameProd(shop)).join('');
-  shopSelect.innerHTML = selectUniques;
+  }
 }
 
 const getShopNameProd = (shopName) => {
@@ -52,10 +53,10 @@ const getShopNameProd = (shopName) => {
   `
 }
 
-function init() {
-  getProducts();
-  getCategory();
-  getShopName();
+async function init() {
+  await getProducts();
+  await getCategory();
+  await getShopName();
 }
 
 init();
